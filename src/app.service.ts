1a718fc0d9a30a60855c7d8bf33d9b3d17f1ec11
Kallesh B S <kalleshbs47@gmail.com>
2025-03-29 11:54:23 +0530

    modified again for PR

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return {message:"hello world! for pr change"};
+    return {message:"hello world!"};
   }
 }
