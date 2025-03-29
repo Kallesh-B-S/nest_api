9dcfe39e86303dab44e993ecbcb3b937b53519d4
Kallesh B S <kalleshbs47@gmail.com>
2025-03-29 11:21:45 +0530

    changed greet message to check auto deploy with pr

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return {message:"hello world!"};
+    return {message:"hello world! for pr check"};
   }
 }
